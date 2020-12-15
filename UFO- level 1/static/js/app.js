63f3aa87c8b6c981f92de21ab366c981cd09992f
prasanna0913 <dspprasanna0913@gmail.com>
2020-12-15 01:19:14 -0500

    working on app.js file

--- UFO- level 1/static/js/app.js
+++ UFO- level 1/static/js/app.js
@@ -3,4 +3,11 @@ var tableData = data;
 
 var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+// Console.log the weather data from data.js
+console.log(tableData);
+
+// Step 1: Loop Through `data` and console.log each ufo data object
+data.forEach(function(ufoData) {
+  console.log(ufoData);
+});
+
