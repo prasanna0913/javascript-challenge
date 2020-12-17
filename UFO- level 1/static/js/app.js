65a9c182a94cf08ee24c79f03a5ef200db06b7d7
prasanna0913 <dspprasanna0913@gmail.com>
2020-12-17 03:32:50 -0500

    updating the app.js

--- UFO- level 1/static/js/app.js
+++ UFO- level 1/static/js/app.js
@@ -16,32 +16,4 @@ tableData.forEach(function(ufoData) {
 });
 });
 
-// Select the button
-var button = d3.select("#filter-btn");
-// Select the form
-var form = d3.select("#form")
-
-// Create event handlers 
-button.on("click", filterData);
-form.on("submit", filterData);
-
-function filterData(){
-      // Prevent the page from refreshing
-    d3.event.preventDefault();
-    d3.selectAll("td").remove();
-    var userInput = d3.select("#datetime");
-    var userData= userInput.property("value");
-    tableData.forEach(function(ufoData) {
-            var filteredData =   tableData.filter(ufoData => ufoData.datetime === userData);
-            console.log(filteredData);
-            filteredData.forEach(function(ufoData) {
-            var row = tbody.append("tr")
-            Object.entries(ufoData).forEach(function([key,value]) {
-                console.log(key, value);
-                var cell = row.append("td");
-                cell.text(value)
-        });
-    });
-    });  
-}
 
