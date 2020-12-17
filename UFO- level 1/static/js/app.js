f672d8d61391cbe1b87ad93da8f31d1ea7b3e5cf
prasanna0913 <dspprasanna0913@gmail.com>
2020-12-17 03:29:34 -0500

    modified index file and app.js

--- UFO- level 1/static/js/app.js
+++ UFO- level 1/static/js/app.js
@@ -6,16 +6,7 @@ var tbody = d3.select("tbody");
 // Console.log the weather data from data.js
 console.log(tableData);
 
-// Select the button
-var button = d3.select("#filter-btn");
-var form = d3.select(".my-new-form")
 
-// Create event handlers 
-button.on("click", updateTable);
-form.on("submit", updateTable);
-
-
-// Select the form
 tableData.forEach(function(ufoData) {
      var row = tbody.append("tr")
     Object.entries(ufoData).forEach(function([key,value]) {
@@ -25,24 +16,32 @@ tableData.forEach(function(ufoData) {
 });
 });
 
-function updateTable(){
-    d3.selectAll("td").remove();
+// Select the button
+var button = d3.select("#filter-btn");
+// Select the form
+var form = d3.select("#form")
 
+// Create event handlers 
+button.on("click", filterData);
+form.on("submit", filterData);
+
+function filterData(){
+      // Prevent the page from refreshing
+    d3.event.preventDefault();
+    d3.selectAll("td").remove();
     var userInput = d3.select("#datetime");
     var userData= userInput.property("value");
-   
-tableData.forEach(function(ufoData) {
-        var filteredData =   tableData.filter(ufoData => ufoData.datetime === userData);
-        console.log(filteredData);
-        filteredData.forEach(function(ufoData) {
-        var row = tbody.append("tr")
-        Object.entries(ufoData).forEach(function([key,value]) {
-            console.log(key, value);
-            var cell = row.append("td");
-            cell.text(value)
+    tableData.forEach(function(ufoData) {
+            var filteredData =   tableData.filter(ufoData => ufoData.datetime === userData);
+            console.log(filteredData);
+            filteredData.forEach(function(ufoData) {
+            var row = tbody.append("tr")
+            Object.entries(ufoData).forEach(function([key,value]) {
+                console.log(key, value);
+                var cell = row.append("td");
+                cell.text(value)
+        });
     });
-});
-
-});  
+    });  
 }
 
