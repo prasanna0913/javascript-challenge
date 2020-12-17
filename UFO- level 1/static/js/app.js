ed628717ad7d64dfeb3f6caf0ae30ae488e0c687
prasanna0913 <dspprasanna0913@gmail.com>
2020-12-17 02:23:14 -0500

    updated the index.html

--- UFO- level 1/static/js/app.js
+++ UFO- level 1/static/js/app.js
@@ -6,6 +6,16 @@ var tbody = d3.select("tbody");
 // Console.log the weather data from data.js
 console.log(tableData);
 
+// Select the button
+var button = d3.select("#filter-btn");
+var form = d3.select(".my-new-form")
+
+// Create event handlers 
+button.on("click", updateTable);
+form.on("submit", updateTable);
+
+
+// Select the form
 tableData.forEach(function(ufoData) {
      var row = tbody.append("tr")
     Object.entries(ufoData).forEach(function([key,value]) {
@@ -14,3 +24,5 @@ tableData.forEach(function(ufoData) {
         cell.text(value)
 });
 });
+
+
